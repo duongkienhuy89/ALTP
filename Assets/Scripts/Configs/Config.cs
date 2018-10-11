0f96d697aad4153c3122e3f8ac7c6d7c7a3ffcb6
B dam <khoatyen2012@gmail.com>
2018-10-11 11:17:11 +0700

    doi thay data va pub

--- Assets/Scripts/Configs/Config.cs
+++ Assets/Scripts/Configs/Config.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Config  {
 
 #if UNITY_IPHONE
+	public static string appId = "ca-app-pub-3940256099942544~3347511713";
 	public static string adsInIDGameOver = "ca-app-pub-2127327600096597/2550957266";
 	public static string adsInIDBanner = "ca-app-pub-2127327600096597/8097963269";
 
@@ -11,9 +12,9 @@ public class Config  {
 
 #if UNITY_ANDROID
 
-	public static string appId = "ca-app-pub-3940256099942544~3347511713";
-	public static string adsInIDGameOver = "ca-app-pub-3940256099942544/1033173712";
-	public static string adsInIDBanner = "ca-app-pub-3940256099942544/6300978111";
+	public static string appId = "ca-app-pub-5148482490300491~7632348443";
+	public static string adsInIDGameOver = "ca-app-pub-5148482490300491/9660456424";
+	public static string adsInIDBanner = "ca-app-pub-5148482490300491/5517746020";
   
 
 
