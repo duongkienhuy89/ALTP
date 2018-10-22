3f97266f7ae86979a5e3e14e024c444c5e20f371
B dam <khoatyen2012@gmail.com>
2018-10-22 16:02:51 +0700

    Data base

--- Assets/Scripts/Configs/Config.cs
+++ Assets/Scripts/Configs/Config.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Config  {
 
 #if UNITY_IPHONE
-	public static string appId = "ca-app-pub-3940256099942544~3347511713";
+	public static string appId = "ca-app-pub-2127327600096597~5144496862";
 	public static string adsInIDGameOver = "ca-app-pub-2127327600096597/2550957266";
 	public static string adsInIDBanner = "ca-app-pub-2127327600096597/8097963269";
 
